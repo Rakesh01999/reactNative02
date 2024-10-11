2cc40692d3a49883f0ce42de67498f6077900aab
Rakesh Biswas <rbiswas01999@gmail.com>
2024-10-11 23:28:19 +0600

    file modification added

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createStackNavigator} from "@react-navigation/stack"
 import Home from '../screeens/Home/Home';
+import Profile from '../screeens/Profile/Profile';
 
 const Stack = createStackNavigator();
 
@@ -8,7 +9,7 @@ const MainNavigation = () => {
     return (
         <Stack.Navigator>
             <Stack.Screen name={'Home'} component={Home} />
-            <Stack.Screen name={'Profile'} component={Home} />
+            <Stack.Screen name={'Profile'} component={Profile} />
         </Stack.Navigator>
     )
 };
